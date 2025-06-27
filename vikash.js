15130004a87d9e8352a7e28f962cc0fc3fe88d71
rampal <rampal24071996@gmail.com>
2025-06-27 15:41:55 +0530

    Updated code with new changes

--- vikash.js
+++ vikash.js
@@ -88,24 +88,7 @@ modalCloses.forEach((modalClose) => {
 
 //Swiper Testimonial
 
-let swiper = new Swiper(".testimonials-container", {
-    spaceBetween: 24,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-        576: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 48,
-        },
-    },
-});
+
 
 // Input Animation
 
@@ -167,3 +150,6 @@ if(navClose) {
     })
 }
 
+// ============================= testo start ==========
+
+// ============================= testo end ==========
